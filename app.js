254657b4793e50ad03a768feb97932d06d4aa934
CharlesAMoss <charlesamoss@yahoo.com>
2014-07-18 15:51:26 -0700

    added app.js and mark up in index.html
    reusing same css from jq ver, so it’s a little wonky but works

---
+++ app.js
@@ -0,0 +1,17 @@
+(function () {
+    var app = angular.module('profile', []);
+
+app.controller("TabController", function () {
+        this.tab = 1;
+
+        this.selectTab = function (setTab) {
+            this.tab = setTab;
+        };
+        this.isSelected = function (checkTab) {
+            return this.tab === checkTab;
+        };
+    });
+
+
+
+})();
