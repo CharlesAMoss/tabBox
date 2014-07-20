65126404d6b5cd9205ecb90d8d47aaa4b1a9a64f
CharlesAMoss <charlesamoss@yahoo.com>
2014-07-20 15:21:29 -0700

    added some content

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 (function () {
     var app = angular.module('profile', []);
 
+
+app.controller('ProfileController', function () {
+        this.products = hero;
+    }); 
+/*
+app.controller('StoreController', function () {
+        this.products = gems;
+    });*/
+
+
 app.controller("TabController", function () {
         this.tab = 1;
 
@@ -12,6 +22,39 @@ app.controller("TabController", function () {
         };
     });
 
-
+var hero = [{
+        name: 'Green Arrow',
+        altName: 'The Emerald Archer',
+        alias: 'Oliver Queen',
+        dayJob: 'playboy billionaire',
+        power: 'human',
+        weapons: 'trick arrows',
+        sidekick: 'Speedy',
+        relationship: 'Black Canary',
+        images: 'img/greenarrow.jpg',            
+        bio: ''
+    }, {
+    	name: 'Green Lantern',
+        altName: '',
+        alias: 'Hal Jordan',
+        dayJob: 'test pilot',
+        power: 'human',
+        weapons: 'power ring',
+        sidekick: 'n/a',
+        relationship: 'Carol Ferris',
+        images: 'img/greenlantern.jpg',
+        bio: ''
+	}, {
+        name: 'The Flash',
+        altName: '',
+        alias: 'Barry Allen',
+        dayJob: 'playboy billionaire',
+        power: 'meta-human',
+        weapons: 'Super Speed',
+        sidekick: 'Wally West',
+        relationship: 'n/a',
+        images: 'img/theflash.jpg',
+        bio: ''
+    }];
 
 })();
